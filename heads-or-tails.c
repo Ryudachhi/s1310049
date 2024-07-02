dc306f6fd5c00999b69549c45e57bd15e55e649e
Ryudachhi <s1310049@u-aizu.ac.jp>
2024-07-03 01:32:45 +0900

    Final revision

--- heads-or-tails.c
+++ heads-or-tails.c
@@ -33,9 +33,9 @@ int main() {
     // 結果の出力
     printf("Heads: %d, Tails: %d\n", heads, tails);
     if(heads > tails){
-        printf("You won!\n");
+        printf("%s won!\n",name);
     } else{
-        printf("You lost!\n");
+        printf("%s lost!\n",name);
     }
 
     return 0;
