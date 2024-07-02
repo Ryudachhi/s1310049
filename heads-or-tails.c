43965891126bad256113080b0872eec4249bf4d8
Ryudachhi <s1310049@u-aizu.ac.jp>
2024-07-03 00:55:38 +0900

    HoT: initial revision

---
+++ heads-or-tails.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+    // 乱数の初期化
+    srand(time(NULL));
+
+    // 結果の格納用変数
+    int heads = 0, tails = 0;
+
+    printf("Tossing a coin...\n");
+
+    // 3ラウンドのコインフリップ
+    for (int i = 1; i <= 3; i++) {
+        int result = rand() % 2;  // 0=表, 1=裏
+        printf("Round %d: ", i);
+        if (result == 0) {
+            printf("Heads\n");
+            heads++;
+        } else {
+            printf("Tails\n");
+            tails++;
+        }
+    }
+
+    // 結果の出力
+    printf("Heads: %d, Tails: %d\n", heads, tails);
+
+    return 0;
+}
